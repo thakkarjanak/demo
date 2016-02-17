e7f90222ac237d41b1be57b29d1be05314cc252e
thakkarjanak <thakkarjanak1811@gmail.com>
2016-02-17 14:34:29 +0530

    Demo Project Commit First Time

---
+++ src/main/java/HelloWorldMaven.java
@@ -0,0 +1,14 @@
+import javax.net.ssl.SSLSocket;
+
+/**
+ * Created by Maulik.M on 2/17/2016.
+ */
+public class HelloWorldMaven {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World through Maven");
+
+
+    }
+}
